b2f6128f29fd9065e04f01c3c039b6efe539d383
bekturisaev <bektur.isaev2004@gmail.com>
2024-01-04 21:14:36 -0600

    new file was created

--- src/test/java/NewClass.java
+++ src/test/java/NewClass.java
@@ -1,6 +1,5 @@
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("Hello World! Bro I changed this file
-just for fun");
+        System.out.println("Hello World! Bro I changed this file just for fun");
     }
 }
