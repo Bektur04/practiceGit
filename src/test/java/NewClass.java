78b300a92d15ef073ae5f88e0f3de82a8cf9516b
Bektur04 <144302281+Bektur04@users.noreply.github.com>
2024-01-04 21:15:39 -0600

    Update NewClass.java
    I added more codes

--- src/test/java/NewClass.java
+++ src/test/java/NewClass.java
@@ -1,5 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello World! Bro I changed this file just for fun");
+        System.out.println("Hello USA);
     }
 }
